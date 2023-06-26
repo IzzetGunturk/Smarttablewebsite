cfbbcafe3218c678e350fe9948bf611c992e7616
IzzetGunturk <izzetgunturk2@hotmail.com>
2023-06-26 19:53:23 +0200

    Added README

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,34 +2,34 @@ import React from 'react';
 
 function Footer() {
 return (
-  <footer class="bg-gray-800 mt-16">
-    <div class="mx-auto max-w-5xl px-4 py-10 sm:px-6 lg:px-8">
-      <div class="flex justify-center">
+  <footer className="bg-gray-800 mt-16">
+    <div className="mx-auto max-w-5xl px-4 py-10 sm:px-6 lg:px-8">
+      <div className="flex justify-center">
         <a href='#hero' className="text-white font-bold text-3xl">SMART TABLE.</a>
       </div>
 
-      <p class="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-400">
+      <p className="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-400">
         Empowering Productivity, Silencing Distractions
       </p>
 
-      <ul class="mt-8 flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
+      <ul className="mt-8 flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
         <li>
-          <a class="text-gray-300 hover:bg-gray-700 transition duration-200 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-            href="/">
+          <a className="text-gray-300 hover:bg-gray-700 transition duration-200 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            href="#concept">
             Concept
           </a>
         </li>
 
         <li>
-          <a class="text-gray-300 hover:bg-gray-700 transition duration-200 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-            href="/">
+          <a className="text-gray-300 hover:bg-gray-700 transition duration-200 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+            href="#prototype">
             Prototype
           </a>
         </li>
       </ul>
 
       <div>
-        <p class="pt-10 text-center justify-center text-sm text-gray-500 lg:mt-0">
+        <p className="pt-10 text-center justify-center text-sm text-gray-500 lg:mt-0">
           Copyright &copy; 2023. All rights reserved.
         </p>
       </div>
